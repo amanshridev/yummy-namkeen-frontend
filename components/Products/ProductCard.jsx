2779ddd6048b6c1c095612ab14b71043e3a06fdd
amanshridev <mailtoamanshri@proton.me>
2023-06-04 02:15:35 +0530

    getting data from firebase success

--- components/Products/ProductCard.jsx
+++ components/Products/ProductCard.jsx
@@ -1,38 +1,58 @@
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Icon } from '@iconify/react';
+import PropTypes from 'prop-types';
 
 
-const ItemCard = () => {
+
+// eslint-disable-next-line react/prop-types
+const ItemCard = ({ item }) => {
 
     return (
-        <div className="card w-72 bg-base-100 border border-[#FE9400] rounded-md mt-8">
+        <div key={item.id} className="card w-72 bg-base-100 border border-[#FE9400] rounded-md mt-8 bg-inherit">
             <figure>
-                <img src="https://dummyimage.com/600x400/000/fff" className='px-5 pt-5 rounded-[1.75rem]' alt="Shoes" />
+                {item.imageUrl && (
+                    <img
+                        src={item.id ? item.imageUrl : "https://dummyimage.com/600x400/000/fff" }
+                        alt={item.itemName}
+                        className="w-full h-44 p-2 object-cover rounded-xl"
+                        width={40}
+                        height={40}
+                    />
+                )}
                 <Icon icon="mdi:fire" color="#ff5440" className='absolute top-[25px] right-6 text-2xl' />
             </figure>
             <div className="card-body p-3">
                 <div className='flex'>
-                    <h2 className="card-title text-center m-auto text-[#FF0505]">Name</h2>
+                    <h2 className="card-title text-center m-auto text-[#FF0505]">{item.itemName}</h2>
                     <span className='relative top-[5px] right-5 text-xl'>
                         <Icon icon="mdi:cards-heart" color="#ff5440" />
                     </span>
                 </div>
                 <p className="card-title text-center m-auto">
-                    <Icon icon="material-symbols:currency-rupee-rounded" /> 100
+                    <Icon icon="material-symbols:currency-rupee-rounded" /> {item.price}
                 </p>
-                <select className="select select-bordered border-orange-500 w-full max-w-xs">
+                <select className="select select-bordered border-orange-500 w-full max-w-xs bg-inherit">
                     <option disabled selected>Select</option>
                     <option>200 g</option>
                     <option>500 g</option>
                     <option>1 kg</option>
                 </select>
                 <div className="card-actions justify-center mt-2">
-                    <button className="btn bg-red-500 border-none hover:bg-orange-500 w-44"> <Icon icon="material-symbols:shopping-cart" color="white" /> &nbsp; Add to cart</button>
+                    <button className="btn bg-red-500 border-none hover:bg-orange-500 w-44 text-white"> <Icon icon="material-symbols:shopping-cart" color="white" /> &nbsp; Add to cart</button>
                 </div>
             </div>
         </div>
     )
 }
+
+ItemCard.propTypes = {
+    item: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string,
+      itemName: PropTypes.string,
+      price: PropTypes.string,
+      description: PropTypes.string,
+    }).isRequired,
+  };
 
 export default ItemCard
